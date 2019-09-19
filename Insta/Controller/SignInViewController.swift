9d2df6928d9fa992a61123ca96aacd4b736a7191
Alexander Kolbin <kolbin_aa@yahoo.com>
2019-09-18 21:50:43 -0400

    feat: Improve startVC interface

---
+++ Insta/Controller/SignInViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SignInViewController.swift
+//  Insta
+//
+//  Created by Apple User on 9/18/19.
+//  Copyright © 2019 Apple User. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        emailTextField.backgroundColor = .clear
+        emailTextField.tintColor = .white
+        emailTextField.textColor = .white
+        emailTextField.attributedPlaceholder = NSAttributedString(string: emailTextField.placeholder!, attributes: [NSAttributedString.Key.foregroundColor : UIColor(white: 1.0, alpha: 0.6)])
+        let bottomLayerEmail = CALayer()
+        bottomLayerEmail.frame = CGRect(x: 0, y: 29, width: emailTextField.frame.width, height: 0.85)
+        bottomLayerEmail.backgroundColor = UIColor(red: 50/255, green: 50/255, blue: 25/255, alpha: 1).cgColor
+        emailTextField.layer.addSublayer(bottomLayerEmail)
+        
+        passwordTextField.backgroundColor = .clear
+        passwordTextField.tintColor = .white
+        passwordTextField.textColor = .white
+        passwordTextField.attributedPlaceholder = NSAttributedString(string: passwordTextField.placeholder!, attributes: [NSAttributedString.Key.foregroundColor : UIColor(white: 1.0, alpha: 0.6)])
+        let bottomLayerPassword = CALayer()
+        bottomLayerPassword.frame = CGRect(x: 0, y: 29, width: passwordTextField.frame.width, height: 0.85)
+        bottomLayerPassword.backgroundColor = UIColor(red: 50/255, green: 50/255, blue: 25/255, alpha: 1).cgColor
+        passwordTextField.layer.addSublayer(bottomLayerPassword)
+    }
+}
---
+++ Insta/Controller/SignUpViewController.swift
@@ -0,0 +1,55 @@
+//
+//  SignUpViewController.swift
+//  Insta
+//
+//  Created by Apple User on 9/18/19.
+//  Copyright © 2019 Apple User. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        usernameTextField.backgroundColor = .clear
+        usernameTextField.tintColor = .white
+        usernameTextField.textColor = .white
+        usernameTextField.attributedPlaceholder = NSAttributedString(string: usernameTextField.placeholder!, attributes: [NSAttributedString.Key.foregroundColor : UIColor(white: 1.0, alpha: 0.6)])
+        let bottomLayerUsername = CALayer()
+        bottomLayerUsername.frame = CGRect(x: 0, y: 29, width: usernameTextField.frame.width, height: 0.85)
+        bottomLayerUsername.backgroundColor = UIColor(red: 50/255, green: 50/255, blue: 25/255, alpha: 1).cgColor
+        usernameTextField.layer.addSublayer(bottomLayerUsername)
+        
+        emailTextField.backgroundColor = .clear
+        emailTextField.tintColor = .white
+        emailTextField.textColor = .white
+        emailTextField.attributedPlaceholder = NSAttributedString(string: emailTextField.placeholder!, attributes: [NSAttributedString.Key.foregroundColor : UIColor(white: 1.0, alpha: 0.6)])
+        let bottomLayerEmail = CALayer()
+        bottomLayerEmail.frame = CGRect(x: 0, y: 29, width: emailTextField.frame.width, height: 0.85)
+        bottomLayerEmail.backgroundColor = UIColor(red: 50/255, green: 50/255, blue: 25/255, alpha: 1).cgColor
+        emailTextField.layer.addSublayer(bottomLayerEmail)
+        
+        passwordTextField.backgroundColor = .clear
+        passwordTextField.tintColor = .white
+        passwordTextField.textColor = .white
+        passwordTextField.attributedPlaceholder = NSAttributedString(string: passwordTextField.placeholder!, attributes: [NSAttributedString.Key.foregroundColor : UIColor(white: 1.0, alpha: 0.6)])
+        let bottomLayerPassword = CALayer()
+        bottomLayerPassword.frame = CGRect(x: 0, y: 29, width: passwordTextField.frame.width, height: 0.85)
+        bottomLayerPassword.backgroundColor = UIColor(red: 50/255, green: 50/255, blue: 25/255, alpha: 1).cgColor
+        passwordTextField.layer.addSublayer(bottomLayerPassword)
+        
+        imageView.layer.cornerRadius = 50
+        imageView.clipsToBounds = true
+    }
+    
+    @IBAction func dismissByClick(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
