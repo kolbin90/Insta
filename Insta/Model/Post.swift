a79d2d8d6754c82ec343fad33d21866f8268299e
Alexander Kolbin <kolbin_aa@yahoo.com>
2019-10-07 17:35:05 -0400

    feat: Create class Post

---
+++ Insta/Model/Post.swift
@@ -0,0 +1,18 @@
+//
+//  Post.swift
+//  Insta
+//
+//  Created by Apple User on 10/7/19.
+//  Copyright © 2019 Apple User. All rights reserved.
+//
+
+import Foundation
+
+class post {
+    var caption: String
+    var photoUrlString: String
+    init(captionText: String, photoUrlStringText: String) {
+        caption = captionText
+        photoUrlString = photoUrlStringText
+    }
+}
