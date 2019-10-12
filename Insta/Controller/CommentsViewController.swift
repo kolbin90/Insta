8d709d53d6cf851a93d02930cf86f09e5f437a85
Alexander Kolbin <kolbin_aa@yahoo.com>
2019-10-12 03:09:06 -0400

    feat: Send comments to database

--- Insta/Controller/CommentsViewController.swift
+++ Insta/Controller/CommentsViewController.swift
@@ -7,27 +7,65 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseDatabase
 
 class CommentsViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var commentTextField: UITextField!
+    @IBOutlet weak var sendButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        sendButton.isEnabled = false
+        handleTextField()
+        tableView.keyboardDismissMode = .onDrag
     }
     
     override func viewWillAppear(_ animated: Bool) {
         tabBarController?.tabBar.isHidden = true
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        tableView.endEditing(true)
     }
-    */
+    
+    
+    func handleTextField() {
+        commentTextField.addTarget(self, action: #selector(self.textFieldDidChange), for: UIControl.Event.editingChanged)
+    }
+    
+    @objc func textFieldDidChange() {
+        guard let comment  = commentTextField.text, !comment.isEmpty else {
+            sendButton.setTitleColor(.lightGray, for: .normal)
+            sendButton.isEnabled = false
+            return
+        }
+        sendButton.setTitleColor(.black, for: .normal)
+        sendButton.isEnabled = true
+    }
+    
 
+    
+    func sendCommentInfoToDatabase(){
+        let ref = Database.database().reference()
+        let commentsRef = ref.child("comments")
+        let newCommentRef = commentsRef.childByAutoId()
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        newCommentRef.setValue(["commentText":commentTextField.text!,"uid":uid]) { (error, ref) in
+            if error != nil {
+                ProgressHUD.showError(error!.localizedDescription)
+                return
+            }
+            self.commentTextField.text = ""
+            self.sendButton.setTitleColor(.lightGray, for: .normal)
+            self.sendButton.isEnabled = false
+        }
+    }
+    
+    @IBAction func sendBtn_TchUpIns(_ sender: Any) {
+        sendCommentInfoToDatabase()
+    }
 }
