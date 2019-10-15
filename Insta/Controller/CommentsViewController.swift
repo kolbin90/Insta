48088a50e7228fd5bfd1e54e61d064a1d48e685b
Alexander Kolbin <kolbin_aa@yahoo.com>
2019-10-15 17:39:02 -0400

    feat: Adjust textfield and send btn to the keyboard height

--- Insta/Controller/CommentsViewController.swift
+++ Insta/Controller/CommentsViewController.swift
@@ -15,6 +15,7 @@ class CommentsViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var commentTextField: UITextField!
     @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var constrainToButtom: NSLayoutConstraint!
     
     let postId = "-Lqj-9vmMg5MdkslU7MO"
     var users = [User]()
@@ -28,15 +29,39 @@ class CommentsViewController: UIViewController {
         sendButton.isEnabled = false
         handleTextField()
         loadComments()
-        tableView.keyboardDismissMode = .onDrag
+        //tableView.keyboardDismissMode = .onDrag
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        tapGesture.cancelsTouchesInView = true
+        tableView.addGestureRecognizer(tapGesture)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_ : )), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_ : )), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         tabBarController?.tabBar.isHidden = true
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        tableView.endEditing(true)
+
+    @objc func hideKeyboard() {
+        view.endEditing(true)
+    }
+    @objc func keyboardWillShow(_ notification : NSNotification) {
+        if let keyboardFrame = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as AnyObject).cgRectValue {
+            UIView.animate(withDuration: 0.3) {
+                print(keyboardFrame)
+                self.constrainToButtom.constant = keyboardFrame.height - self.view.safeAreaInsets.bottom
+                print(self.constrainToButtom.constant)
+                self.view.layoutIfNeeded()
+            }
+        }
+    }
+    @objc func keyboardWillHide(_ notification : NSNotification) {
+        UIView.animate(withDuration: 0.3) {
+            self.constrainToButtom.constant = 0
+            self.view.layoutIfNeeded()
+        }
     }
     
     func loadComments() {
@@ -102,6 +127,8 @@ class CommentsViewController: UIViewController {
             self.commentTextField.text = ""
             self.sendButton.setTitleColor(.lightGray, for: .normal)
             self.sendButton.isEnabled = false
+            self.view.endEditing(true)
+
         }
     }
     
