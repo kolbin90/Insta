bdcb568fa294e429d799fd03bdb6711fe92b2d1d
Alexander Kolbin <kolbin_aa@yahoo.com>
2019-11-01 17:24:39 -0400

    feat: Make SerachVC conform TableViewDataSouce

--- Insta/Controller/SearchViewController.swift
+++ Insta/Controller/SearchViewController.swift
@@ -9,16 +9,53 @@
 import UIKit
 
 class SearchViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     var searchBar = UISearchBar()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationController?.navigationBar.tintColor = .black
+        
         searchBar.searchBarStyle = .minimal
         searchBar.placeholder = "Search"
         searchBar.frame.size.width = view.frame.size.width - 60
+        searchBar.delegate = self
+        
         let searchItem = UIBarButtonItem(customView: searchBar)
         navigationItem.rightBarButtonItem = searchItem
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        tapGesture.cancelsTouchesInView = true
+        tableView.addGestureRecognizer(tapGesture)
+        tableView.dataSource = self
     }
+    
+    @objc func hideKeyboard() {
+        searchBar.endEditing(true)
+    }
+}
 
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print(searchBar.text)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchBar.text)
+    }
+}
 
+extension SearchViewController:  UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PeopleCell", for: indexPath) as! PeopleCell
+
+        return cell
+    }
 }
