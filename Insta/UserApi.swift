9c04e8503b3e136899651aedc57b39a549871a27
Alexander Kolbin <kolbin_aa@yahoo.com>
2019-11-01 16:40:04 -0400

    feat: Don't return current user when observing users

--- Insta/UserApi.swift
+++ Insta/UserApi.swift
@@ -37,7 +37,9 @@ class UserApi {
         REF_USERS.observe(.childAdded) { (snapshot) in
             if let dict = snapshot.value as? [String: Any] {
                 let user = UserModel.transformToUser(dict: dict, key: snapshot.key)
-                completion(user)
+                if user.id != Api.user.CURRENT_USER!.uid {
+                    completion(user)
+                }
             }
         }
     }
