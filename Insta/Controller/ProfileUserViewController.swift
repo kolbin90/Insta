614866bfe587d8b0cd7e98226f725400d4e3283d
Alexander Kolbin <kolbin_aa@yahoo.com>
2019-11-02 21:54:43 -0400

    feat: Design ProfileUserVC and create class

---
+++ Insta/Controller/ProfileUserViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileUserViewController.swift
+//  Insta
+//
+//  Created by Apple User on 11/2/19.
+//  Copyright © 2019 Apple User. All rights reserved.
+//
+
+import UIKit
+
+class ProfileUserViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
