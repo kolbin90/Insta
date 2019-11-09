d312b50ee4bf252524edfd8e1942bd5415dd187c
Alexander Kolbin <kolbin_aa@yahoo.com>
2019-11-09 03:23:27 -0500

    feat: Show user information in SettingVC

--- Insta/Controller/SettingTableViewController.swift
+++ Insta/Controller/SettingTableViewController.swift
@@ -9,17 +9,35 @@
 import UIKit
 
 class SettingTableViewController: UITableViewController {
-
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         navigationController?.navigationBar.tintColor = .black
         navigationItem.title = "Edit Profile"
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        fetchCurrentUser()
     }
-
+    
+    func fetchCurrentUser() {
+        Api.user.observeCurrentUser { (user) in
+            self.usernameTextField.text = user.username
+            self.emailTextField.text = user.email
+            if let profileImageUrl = URL(string: user.profileImageUrl!) {
+                self.profileImageView.sd_setImage(with: profileImageUrl, completed: nil)
+            }
+        }
+    }
+    
+    @IBAction func changeImageBtn_TchUpIns(_ sender: Any) {
+    }
+    @IBAction func saveBtn_TchUpIns(_ sender: Any) {
+    }
+    @IBAction func logoutBtn_TchUpIns(_ sender: Any) {
+    }
+    
 }
